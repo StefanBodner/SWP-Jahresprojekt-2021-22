09c4ffdef45b48ed530a4892c13949624082e940
Stefan Bodner <stefan.bodner@hak-kitz.ac.at>
2021-10-07 09:04:27 +0200

    07.10.2021 - 09:03

--- MyTrade/Form1.Designer.cs
+++ MyTrade/Form1.Designer.cs
@@ -29,20 +29,47 @@ namespace MyTrade
         /// </summary>
         private void InitializeComponent()
         {
+            this.btn_data = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // btn_data
+            // 
+            this.btn_data.Location = new System.Drawing.Point(357, 285);
+            this.btn_data.Name = "btn_data";
+            this.btn_data.Size = new System.Drawing.Size(75, 23);
+            this.btn_data.TabIndex = 0;
+            this.btn_data.Text = "Get Data";
+            this.btn_data.UseVisualStyleBackColor = true;
+            this.btn_data.Click += new System.EventHandler(this.btn_data_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(173, 86);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(444, 148);
+            this.textBox1.TabIndex = 1;
             // 
             // frm_main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btn_data);
             this.Name = "frm_main";
-            this.Text = "MyTrade5";
+            this.Text = "MyTrade";
+            this.Load += new System.EventHandler(this.frm_main_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_data;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- MyTrade/Form1.cs
+++ MyTrade/Form1.cs
@@ -16,5 +16,15 @@ namespace MyTrade
         {
             InitializeComponent();
         }
+
+        private void frm_main_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_data_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
