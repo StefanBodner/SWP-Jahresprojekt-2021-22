359fb698bb0b91362e1da21d00f44d7aa3385c2f
Maximilian Hager <maximilian.hager@hak-kitz.ac.at>
2021-11-10 10:40:07 +0100

    Namensänderung

---
+++ MyTrade/Login.Designer.cs
@@ -0,0 +1,133 @@
+﻿
+namespace MyTrade
+{
+    partial class frm_login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_username = new System.Windows.Forms.Label();
+            this.tb_username = new System.Windows.Forms.TextBox();
+            this.tb_password = new System.Windows.Forms.TextBox();
+            this.lbl_password = new System.Windows.Forms.Label();
+            this.pb_logo = new System.Windows.Forms.PictureBox();
+            this.btn_login = new System.Windows.Forms.Button();
+            this.btn_register = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_logo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbl_username
+            // 
+            this.lbl_username.AutoSize = true;
+            this.lbl_username.Location = new System.Drawing.Point(23, 288);
+            this.lbl_username.Name = "lbl_username";
+            this.lbl_username.Size = new System.Drawing.Size(58, 13);
+            this.lbl_username.TabIndex = 0;
+            this.lbl_username.Text = "Username:";
+            // 
+            // tb_username
+            // 
+            this.tb_username.Location = new System.Drawing.Point(26, 304);
+            this.tb_username.Name = "tb_username";
+            this.tb_username.Size = new System.Drawing.Size(100, 20);
+            this.tb_username.TabIndex = 1;
+            // 
+            // tb_password
+            // 
+            this.tb_password.Location = new System.Drawing.Point(26, 349);
+            this.tb_password.Name = "tb_password";
+            this.tb_password.Size = new System.Drawing.Size(100, 20);
+            this.tb_password.TabIndex = 3;
+            // 
+            // lbl_password
+            // 
+            this.lbl_password.AutoSize = true;
+            this.lbl_password.Location = new System.Drawing.Point(23, 333);
+            this.lbl_password.Name = "lbl_password";
+            this.lbl_password.Size = new System.Drawing.Size(56, 13);
+            this.lbl_password.TabIndex = 2;
+            this.lbl_password.Text = "Password:";
+            // 
+            // pb_logo
+            // 
+            this.pb_logo.BackgroundImage = global::MyTrade.Properties.Resources.MyTradeLogo;
+            this.pb_logo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pb_logo.Location = new System.Drawing.Point(43, 39);
+            this.pb_logo.Name = "pb_logo";
+            this.pb_logo.Size = new System.Drawing.Size(214, 203);
+            this.pb_logo.TabIndex = 4;
+            this.pb_logo.TabStop = false;
+            // 
+            // btn_login
+            // 
+            this.btn_login.Location = new System.Drawing.Point(215, 304);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(75, 23);
+            this.btn_login.TabIndex = 5;
+            this.btn_login.Text = "Login";
+            this.btn_login.UseVisualStyleBackColor = true;
+            // 
+            // btn_register
+            // 
+            this.btn_register.Location = new System.Drawing.Point(215, 349);
+            this.btn_register.Name = "btn_register";
+            this.btn_register.Size = new System.Drawing.Size(75, 23);
+            this.btn_register.TabIndex = 6;
+            this.btn_register.Text = "Register";
+            this.btn_register.UseVisualStyleBackColor = true;
+            // 
+            // frm_login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(332, 450);
+            this.Controls.Add(this.btn_register);
+            this.Controls.Add(this.btn_login);
+            this.Controls.Add(this.pb_logo);
+            this.Controls.Add(this.tb_password);
+            this.Controls.Add(this.lbl_password);
+            this.Controls.Add(this.tb_username);
+            this.Controls.Add(this.lbl_username);
+            this.Name = "frm_login";
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.frm_login_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pb_logo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_username;
+        private System.Windows.Forms.TextBox tb_username;
+        private System.Windows.Forms.TextBox tb_password;
+        private System.Windows.Forms.Label lbl_password;
+        private System.Windows.Forms.PictureBox pb_logo;
+        private System.Windows.Forms.Button btn_login;
+        private System.Windows.Forms.Button btn_register;
+    }
+}
